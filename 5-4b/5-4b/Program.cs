d5e4bd26704a615201acacf05b92ebd211769bdf
KPWysocki <59768608+KPWysocki@users.noreply.github.com>
2020-11-28 14:30:51 +0100

    Add files via upload

---
+++ 5-4b/5-4b/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _5_4_a
+{
+    class Program
+    {
+        static void Mnoz(int[] tab, int m)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i] = tab[i] * m;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj elemtny tablicy \nrozmiar: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj elementy które mają być zawarte w tablicy");
+            int[] tab = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                tab[i] = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Podaj mnożnik tablicy: ");
+            int m = int.Parse(Console.ReadLine());
+            Mnoz(tab, m);
+            Console.Write("Po przemnożeniu powstala tablica: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(tab[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 5-5/5-5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _5_5
+{
+    class Program
+    {
+        static void Rysuj(int dl, int sz, char zn)
+        {
+            char[,] tab = new char[dl, sz];
+            for (int j=0; j<sz;j++)
+            {
+                for (int i=0;i<dl;i++)
+                {
+                    tab[i, j] = zn;
+                }
+            }
+            for (int j = 0; j < sz; j++)
+            {
+                for (int i = 0; i < dl; i++)
+                {
+                    Console.Write(tab[i, j]);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine(" ");
+            for (int i = 0; i < dl; i++)
+            {
+                for (int j = 0; j < sz; j++)
+                {
+                    Console.Write(tab[i, j]);
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj wymiary prostokata\ndlugosc: ");
+            int dl = int.Parse(Console.ReadLine());
+            Console.Write("szerokosc:");
+            int sz = int.Parse(Console.ReadLine());
+            Console.Write("Znak, ktorym prostokat ma byc wypelniony: ");
+            char zn = char.Parse(Console.ReadLine());
+            Rysuj(dl, sz, zn);
+        }
+    }
+}
